2d2a8414c83a6ad5cb8a8eaba8c60f05ccfda572
jhonata padua <jhonatanps>
2022-03-18 20:08:34 -0300

    Outra forma de importar e exportar modulos

---
+++ 2_FUNDAMENTOS/2_export_import/index.mjs
@@ -0,0 +1,5 @@
+//importe mais simplificado
+import soma from './meu_modulo.mjs'
+
+soma(3, 2)
+soma(5, 10)
---
+++ 2_FUNDAMENTOS/2_export_import/meu_modulo.mjs
@@ -0,0 +1,6 @@
+//outro padrão para export de modulos.
+function soma(a, b) {
+  console.log(a + b)
+}
+
+export default soma
