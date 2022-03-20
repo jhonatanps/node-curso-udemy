e1fcbe5bdaaeb5f6f1dcc45703310105ec0433f1
jhonata padua <jhonatanps>
2022-03-20 01:42:49 -0300

    Garantido que o numero passado seja inteiro

--- 2_FUNDAMENTOS/6_pratica_com_args/index.js
+++ 2_FUNDAMENTOS/6_pratica_com_args/index.js
@@ -8,7 +8,7 @@ const soma = require('./soma').soma
 
 const args = minimist(process.argv.slice(2))
 
-const a = args['a']
-const b = args['b']
+const a = parseInt(args['a'])
+const b = parseInt(args['b'])
 
 soma(a, b)
