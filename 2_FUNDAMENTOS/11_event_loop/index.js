e2be842ad0c4e4ab85e74eeb3d91b214817b68fa
jhonata padua <jhonatanps>
2022-03-20 19:13:47 -0300

    Event loop

---
+++ 2_FUNDAMENTOS/11_event_loop/index.js
@@ -0,0 +1,13 @@
+function a() {
+  console.log('Executando a()')
+}
+
+function b() {
+  console.log('Executando b()')
+}
+
+function c() {
+  console.log('Executando c()')
+}
+
+//evente loop asegura o sequincimanto de execuçao.
