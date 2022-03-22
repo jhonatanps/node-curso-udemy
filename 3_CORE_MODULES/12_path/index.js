e378a61a8b1e3f4df0a81576e19a3d3ac6c35130
jhonata padua <jhonatanps>
2022-03-22 01:26:29 -0300

    Caminhos e detalhes -> usando o path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = 'caminho do arquivo'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
